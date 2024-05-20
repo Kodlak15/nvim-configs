9a7d1170392bff227e58389ba4e6ee2b4fa9af80
Kodlak15 <stanlcod15@gmail.com>
2024-05-19 20:23:02 -0700

    Initial commit

--- flake.nix
+++ flake.nix
@@ -16,24 +16,46 @@
     nixvim,
     flake-utils,
     ...
-  } @ inputs: let
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+          nixvim' = nixvim.legacyPackages.${system};
+        });
+
     config = import ./config;
-  in
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      nixvim' = nixvim.legacyPackages.${system};
-      nvim = nixvim'.makeNixvimWithModule {
+  in {
+    packages = forEachSupportedSystem ({
+      pkgs,
+      nixvim',
+    }: {
+      default = nixvim'.makeNixvimWithModule {
         inherit pkgs;
         module = config;
         extraSpecialArgs = {
           inherit self;
         };
       };
-    in {
-      packages = {
-        default = nvim;
-      };
-
-      formatter = pkgs.alejandra;
     });
+  };
+
+  # flake-utils.lib.eachDefaultSystem (system: let
+  #   pkgs = import nixpkgs {inherit system;};
+  #   nixvim' = nixvim.legacyPackages.${system};
+  #   nvim = nixvim'.makeNixvimWithModule {
+  #     inherit pkgs;
+  #     module = config;
+  #     extraSpecialArgs = {
+  #       inherit self;
+  #     };
+  #   };
+  # in {
+  #   packages = {
+  #     default = nvim;
+  #   };
+  #
+  #   formatter = pkgs.alejandra;
+  # });
 }
