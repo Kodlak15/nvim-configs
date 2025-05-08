fa73f8ce8a17f30a3ef20a941322a8c6a981895c
Kodlak15 <stanlcod15@protonmail.com>
2025-05-08 14:51:03 -0700

    Add transparency to theme

--- config/colorschemes.nix
+++ config/colorschemes.nix
@@ -49,6 +49,19 @@
       enable = true;
       settings = {
         mirage = true;
+        terminal = false;
+        overrides = {
+          # Transparency
+          Normal = {bg = "None";};
+          NormalFloat = {bg = "none";};
+          ColorColumn = {bg = "None";};
+          SignColumn = {bg = "None";};
+          Folded = {bg = "None";};
+          FoldColumn = {bg = "None";};
+          CursorLine = {bg = "None";};
+          CursorColumn = {bg = "None";};
+          VertSplit = {bg = "None";};
+        };
       };
     };
   };
