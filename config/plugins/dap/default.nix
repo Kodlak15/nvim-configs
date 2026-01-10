c3101f9c08d51e77c088f040bf6c143a012e87a4
Kodlak15 <stanlcod15@protonmail.com>
2026-01-09 19:50:10 -0800

    Improve cpp dap setup

--- config/plugins/dap/default.nix
+++ config/plugins/dap/default.nix
@@ -1,7 +1,27 @@
-{
+{pkgs, ...}: {
   plugins = {
     dap = {
       enable = true;
+      adapters = {
+        executables.lldb = {
+          command = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
+        };
+      };
+      configurations = {
+        cpp = [
+          {
+            # TODO: configure keybinds
+            type = "lldb";
+            request = "launch";
+            name = "lldb";
+            program.__raw = ''
+              function()
+                return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+              end
+            '';
+          }
+        ];
+      };
     };
 
     dap-ui = {
