98c83a4544b253dac5912b3644969e656e87f59b
Kodlak15 <stanlcod15@protonmail.com>
2026-02-17 22:48:05 -0800

    Disable supermaven by default

--- config/plugins/supermaven/default.nix
+++ config/plugins/supermaven/default.nix
@@ -15,6 +15,11 @@
       log_level = "info";
       disable_inline_completion = false;
       disable_keymaps = false;
+      condition.__raw = ''
+        function()
+          return true;
+        end
+      ''; # disable supermaven by default
     };
   };
 }
